274393610811f1a51e22acbb6ab421c34f29d072
Antonio Peixe <antonio.icr.peixe@gmail.com>
2023-05-07 00:13:29 +0100

    prems fixes and update install

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# sudo pacman -Syu ansible git base-devel
-# git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-# git clone git@github.com/antpeixe/ansible && cd ansible
+sudo pacman -Syu ansible git base-devel
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+git clone git@github.com/antpeixe/ansible && cd ansible
 
-ansible-playbook -t install --ask-vault-pass local.yml
+ansible-playbook -t install -K --ask-vault-pass local.yml
 
+packages=("redshift-minimal" "papirus-folders-catppuccin-git")
+echo "Installing extra yay packages: $packages"
+yay -Syu $packages
