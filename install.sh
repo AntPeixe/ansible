bd6f6bc1da0131a6307caaf98af16f224729d569
Antonio Peixe <antonio.icr.peixe@gmail.com>
2023-05-09 16:42:45 +0100

    chore: improve install and add missing packages

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo pacman -Syu ansible git base-devel
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-git clone git@github.com/antpeixe/ansible && cd ansible
+# assumes git is installed
+# after finishing consider removing yay folder and install yay with yay
+sudo pacman -Syu ansible base-devel
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ..
 
 ansible-playbook -t install -K --ask-vault-pass local.yml
 
-packages=(redshift-minimal papirus-folders-catppuccin-git)
+packages=(redshift-minimal papirus-folders-catppuccin-git catppuccin-gtk-theme-mocha brave-bin)
 echo "Installing extra yay packages: ${packages[@]}"
 yay -Syu ${packages[@]}
